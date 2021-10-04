6c2cb4a264d47547d5ca6daa44547335fdb86e10
eabz <eabz@polispay.org>
2021-10-03 21:25:06 -0500

    Fixes

--- subgraphs/market/src/mapping.ts
+++ subgraphs/market/src/mapping.ts
@@ -1,6 +1,16 @@
 import {Buy} from "../generated/Market/Market";
+import {Sale} from "../generated/schema";
 
 export function handleBuy(event: Buy): void {
+    let sale = new Sale(event.params.id.toString() + "_" + event.block.timestamp.toString())
 
+    sale.summoner = event.params.id
+    sale.txid = event.transaction.hash.toHexString()
+    sale.timestamp = event.block.timestamp
+    sale.seller = event.params.seller.toHexString()
+    sale.buyer = event.params.buyer.toHexString()
+    sale.price = event.params.price
+    sale.fee = event.params.fee
 
+    sale.save()
 }
