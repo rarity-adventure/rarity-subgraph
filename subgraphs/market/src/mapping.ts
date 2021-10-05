e14482635897e3c63c89e2dc029c883f765389e8
eabz <eabz@polispay.org>
2021-10-05 10:22:21 -0500

    Add global

--- subgraphs/market/src/mapping.ts
+++ subgraphs/market/src/mapping.ts
@@ -1,5 +1,8 @@
 import {Buy} from "../generated/Market/Market";
-import {Sale} from "../generated/schema";
+import {Sale, Global} from "../generated/schema";
+import { integer } from '@protofire/subgraph-toolkit'
+
+const global_data_id = "globals"
 
 export function handleBuy(event: Buy): void {
     let sale = new Sale(event.params.id.toString() + "_" + event.block.timestamp.toString())
@@ -13,4 +16,19 @@ export function handleBuy(event: Buy): void {
     sale.fee = event.params.fee
 
     sale.save()
+
+    let global = Global.load(global_data_id)
+    if (!global) {
+        global = new Global(global_data_id)
+        global.trades = integer.ZERO
+        global.fees = integer.ZERO
+        global.volume = integer.ZERO
+        global.save()
+    }
+
+    global.trades = global.trades.plus(integer.ONE)
+    global.fees = global.fees.plus(event.params.fee)
+    global.volume = global.volume.plus(event.params.price)
+
+    global.save()
 }
